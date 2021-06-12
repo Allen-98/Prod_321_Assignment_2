b758a08379a7756e19365acd800a7d1673b4b5e3
Xiao7 <63487309+Allen-98@users.noreply.github.com>
2021-06-12 16:51:15 +1200

    Complete Part 1 - Task 7

---
+++ Assets/Part 1 - Advanced Lighting and Global Illumination/Task 7/VisibleControl.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VisibleControl : MonoBehaviour
+{
+
+    public Material mt;
+    [Range(0, 1)]
+    public float alpha;
+    public Slider slider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        alpha = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mt.SetFloat("_Metallic", alpha);
+    }
+
+    public void ChangeValue()
+    {
+        alpha = slider.value;
+
+    }
+
+}
